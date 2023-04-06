f85a1ff36fba8ae9045585b62481625f6489777f
ekkusa <dayesofficial@gmail.com>
2023-04-06 23:10:43 +0800

    akari-kddi: Clean-up tree
    * Move config-related stuff to configs/
    * Drop Lineage.dependencies
    * Drop Lineage overlay due to it not even being in the akari DT

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,15 +16,15 @@ TARGET_SCREEN_DENSITY := 440
 TARGET_KERNEL_CONFIG := tama_akari_kddi_defconfig
 
 # HIDL
-DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/vintf/manifest.xml
 
 # Partitions
 # Reserve space for data encryption (44712771584-16384)
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 44712755200
 
 # Properties
-TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
-TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/configs/props/system.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/configs/props/vendor.prop
 
 # Inherit from the proprietary version
 include vendor/sony/akari/BoardConfigVendor.mk
